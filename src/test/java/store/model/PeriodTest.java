1557a0097fb881a1cc55c67b683fb0342ee6d7ae
HyeonsuLee <leehyeonsu4888@naver.com>
2024-11-11 09:29:43 +0900

    test: 프로모션 진행 여부 테스트 추가

--- src/test/java/store/model/PeriodTest.java
+++ src/test/java/store/model/PeriodTest.java
@@ -1,16 +1,30 @@
 package store.model;
 
-import java.time.LocalDate;
-import org.assertj.core.api.Assertions;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.DateTimes;
 import org.junit.jupiter.api.Test;
 
 class PeriodTest {
 
     @Test
     void 프로모션_기간은_시작일이_종료일보다_클경우_예외가_발생한다() {
-        Assertions.assertThatThrownBy(() -> new Period(
-                LocalDate.now().plusDays(1),
-                LocalDate.now()
+        assertThatThrownBy(() -> new Period(
+                DateTimes.now().plusDays(1).toLocalDate(),
+                DateTimes.now().toLocalDate()
         )).isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void 현재_날짜가_프로모션_진행중인지_반환한다() {
+        assertSimpleTest(() -> {
+            Period period = new Period(
+                    DateTimes.now().minusDays(1).toLocalDate(),
+                    DateTimes.now().toLocalDate()
+            );
+            assertThat(period.isPeriod()).isTrue();
+        });
     }
 }
