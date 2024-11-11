f235a4adeeacc21b220612ca6789d9fa9d7939a9
HyeonsuLee <leehyeonsu4888@naver.com>
2024-11-11 22:36:44 +0900

    test: QuantityTest에서 ParameterizedTest이용

--- src/test/java/store/model/QuantityTest.java
+++ src/test/java/store/model/QuantityTest.java
@@ -4,6 +4,8 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class QuantityTest {
 
@@ -15,21 +17,16 @@ class QuantityTest {
         });
     }
 
-    @Test
-    void 재고보다_적은_수량을_감소시킨다() {
+    @ParameterizedTest
+    @CsvSource({
+            "5, 3, 2",   // 재고보다 적은 수량을 감소시키는 경우
+            "5, 10, 0"   // 재고보다 많은 수량을 감소시키는 경우
+    })
+    void 재고를_감소시킨다(int initialStock, int decreaseBy, int expectedStock) {
         assertSimpleTest(() -> {
-            Stock quantity = new Stock(5);
-            quantity.decreaseBy(3);
-            assertThat(quantity.count()).isEqualTo(2);
-        });
-    }
-
-    @Test
-    void 재고보다_많은_수량을_감소시킨다() {
-        assertSimpleTest(() -> {
-            Stock quantity = new Stock(5);
-            quantity.decreaseBy(10);
-            assertThat(quantity.count()).isZero();
+            Stock quantity = new Stock(initialStock);
+            quantity.decreaseBy(decreaseBy);
+            assertThat(quantity.count()).isEqualTo(expectedStock);
         });
     }
 }
