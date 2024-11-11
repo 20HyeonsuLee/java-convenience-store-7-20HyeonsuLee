567c98c9054e8ada5660b163471c9ea84fe0959c
HyeonsuLee <leehyeonsu4888@naver.com>
2024-11-11 12:05:22 +0900

    test: 필요 없는 테스트 삭제

--- src/test/java/store/model/QuantityTest.java
+++ src/test/java/store/model/QuantityTest.java
@@ -2,17 +2,10 @@ package store.model;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
 class QuantityTest {
-
-    @Test
-    void 재고는_음수일수_없다() {
-        assertThatThrownBy(() -> new Quantity(-1))
-                .isInstanceOf(IllegalStateException.class);
-    }
 
     @Test
     void 재고를_생성한다() {
@@ -36,7 +29,7 @@ class QuantityTest {
         assertSimpleTest(() -> {
             Quantity quantity = new Quantity(5);
             quantity.decreaseBy(10);
-            assertThat(quantity.getCount()).isEqualTo(0);
+            assertThat(quantity.getCount()).isZero();
         });
     }
 }
