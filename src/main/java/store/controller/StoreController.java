07f7feef9d2aef29b4aa28bdbbdc120cc2f7ae74
HyeonsuLee <leehyeonsu4888@naver.com>
2024-11-11 11:50:31 +0900

    feat: 프로모션 적용이 가능한 상품에 대해 수량보다 적게 가져온 경우 추가 여부 입력받는 기능 작성

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -2,6 +2,8 @@ package store.controller;
 
 import java.util.List;
 import java.util.function.Supplier;
+import store.dto.InputProductDTO;
+import store.dto.InputPromotionDTO;
 import store.model.Order;
 import store.service.StoreService;
 import store.view.InputView;
@@ -14,14 +16,32 @@ public class StoreController {
     private final OutputView outputView = new OutputView();
     private final StoreService storeService = new StoreService();
 
+    public void uploadData() {
+        uploadShopData();
+    }
+
     public void run() {
         printProduct();
         List<Order> orders = handleInput(this::inputOrder);
-
+        reOrderPromotion(orders);
     }
 
     private void validateOrdersQuantity(List<Order> orders) {
         orders.forEach(storeService::validateOrderQuantity);
+    }
+
+    private void reOrderPromotion(List<Order> orders) {
+        orders.forEach(order -> {
+            int requiredQuantity = storeService.getRequiredFreeQuantity(order);
+            if (requiredQuantity > 0) {
+                storeService.reOrderPromotion(order, handleInput(() -> inputReOrderPromotion(order, requiredQuantity)));
+            }
+        });
+    }
+
+    private boolean inputReOrderPromotion(Order order, Integer count) {
+        String confirm = inputView.inputAdditionalFreeProduct(order.getName(), count);
+        return Parser.parseConfirm(confirm);
     }
 
     private void printProduct() {
@@ -29,10 +49,24 @@ public class StoreController {
         outputView.printProducts(storeService.getAllProducts());
     }
 
+    private void uploadShopData() {
+        List<InputProductDTO> products = handleInput(this::inputProducts);
+        List<InputPromotionDTO> promotions = handleInput(this::inputPromotions);
+        storeService.uploadData(products, promotions);
+    }
+
     private List<Order> inputOrder() {
         List<Order> orders = Parser.parseOrders(inputView.inputOrder());
         validateOrdersQuantity(orders);
         return orders;
+    }
+
+    private List<InputProductDTO> inputProducts() {
+        return inputView.inputProducts();
+    }
+
+    private List<InputPromotionDTO> inputPromotions() {
+        return inputView.inputPromotions();
     }
 
     private <T> T handleInput(Supplier<T> inputSupplier) {
